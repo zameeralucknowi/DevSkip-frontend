ab0bb9ae48f20fcad40b367e1f9305d6108812d3
zameer <zameeralucknowi@gmail.com>
2025-06-06 02:45:08 +0530

    added backend hosted apis url

--- src/utils/requestMethods.js
+++ src/utils/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = 'http://localhost:3000'
+const BASE_URL = 'https://devskip-apis.onrender.com'
 
 const publicRequest = axios.create({
     baseURL : BASE_URL
